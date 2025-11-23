ccbc6800fdc26c1367b17f84df428eb3701ceb91
Joselio Jr. <joselio.fc@hotmail.com>
2025-11-23 02:00:16 -0300

    ✨ feat: Implementação do cliente-estoque

---
+++ cliente-estoque/cliente-estoque/src/test/java/com/joseliojr/cliente_estoque/ClienteEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseliojr.cliente_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
